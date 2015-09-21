70838c5fdc22341168f7ca553b4bdde128421c58
hy9be_uva <hy9be@virginia.edu>
2015-09-21 10:10:26 -0700

    NSLog info and callstack of unhandled exceptions
    As exception goes to assembly sometimes.

--- LivePhotoDemo/main.m
+++ LivePhotoDemo/main.m
@@ -10,7 +10,13 @@
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    @try {
+        @autoreleasepool {
+            return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        }
+    }
+    @catch (NSException* exception) {
+        NSLog(@"Uncaught exception %@", exception);
+        NSLog(@"Stack trace: %@", [exception callStackSymbols]);
     }
 }
